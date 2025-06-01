17111c185cd7eb8b388a4e03758ee07d25385aba
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-05-31 22:58:57 -0300

    Update segundaquestao.c

--- tentativasAulaExtra/segundaquestao.c
+++ tentativasAulaExtra/segundaquestao.c
@@ -12,83 +12,12 @@ typedef struct {
 } Livro;
 
 
-void preencherVetor(Livro vetor[], int *n) {
-    printf("Quantos livros deseja cadastrar? ");
-    scanf("%d", n);
-    getchar(); 
+void preencherVetor(Livro vetor[], int *n);
+void imprimirVetor(Livro vetor[], int n);
+int buscarPorTitulo(Livro vetor[], int n, char tituloBusca[]);
+void ordenarPorTitulo(Livro vetor[], int n)
+int buscaBinariaPorTitulo(Livro vetor[], int n, char tituloBusca[]);
 
-    for (int i = 0; i < *n; i++) {
-        printf("\nLivro %d:\n", i + 1);
-        printf("Título: ");
-        fgets(vetor[i].titulo, 100, stdin);
-        vetor[i].titulo[strcspn(vetor[i].titulo, "\n")] = '\0';
-
-        printf("Autor: ");
-        fgets(vetor[i].autor, 100, stdin);
-        vetor[i].autor[strcspn(vetor[i].autor, "\n")] = '\0';
-
-        printf("Editora: ");
-        fgets(vetor[i].editora, 100, stdin);
-        vetor[i].editora[strcspn(vetor[i].editora, "\n")] = '\0';
-
-        printf("Ano de Publicação: ");
-        scanf("%d", &vetor[i].anoPublicacao);
-
-        printf("Quantidade de Páginas: ");
-        scanf("%d", &vetor[i].qtdPaginas);
-        getchar(); 
-    }
-}
-
-
-void imprimirVetor(Livro vetor[], int n) {
-    printf("\n--- Lista de Livros ---\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d. %s | %s | %s | %d | %d páginas\n", 
-            i, vetor[i].titulo, vetor[i].autor, vetor[i].editora, 
-            vetor[i].anoPublicacao, vetor[i].qtdPaginas);
-    }
-}
-
-
-int buscarPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
-    for (int i = 0; i < n; i++) {
-        if (strcmp(vetor[i].titulo, tituloBusca) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-
-void ordenarPorTitulo(Livro vetor[], int n) {
-    Livro temp;
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1-i; j++) {
-            if (strcmp(vetor[j].titulo, vetor[j+1].titulo) > 0) {
-                temp = vetor[j];
-                vetor[j] = vetor[j+1];
-                vetor[j+1] = temp;
-            }
-        }
-    }
-}
-
-
-int buscaBinariaPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
-    int esq = 0, dir = n - 1;
-
-    while (esq <= dir) {
-        int meio = (esq + dir) / 2;
-        int cmp = strcmp(vetor[meio].titulo, tituloBusca);
-
-        if (cmp == 0) return meio;
-        else if (cmp < 0) esq = meio + 1;
-        else dir = meio - 1;
-    }
-
-    return -1;
-}
 
 int main() {
     Livro livros[MAX_LIVROS];
@@ -117,4 +46,78 @@ int main() {
     }
 
     return 0;
+}
+
+void preencherVetor(Livro vetor[], int *n) {
+    printf("Quantos livros deseja cadastrar? ");
+    scanf("%d", n);
+    getchar(); 
+
+    for (int i = 0; i < *n; i++) {
+        printf("\nLivro %d:\n", i + 1);
+        printf("Título: ");
+        fgets(vetor[i].titulo, 100, stdin);
+        vetor[i].titulo[strcspn(vetor[i].titulo, "\n")] = '\0';
+
+        printf("Autor: ");
+        fgets(vetor[i].autor, 100, stdin);
+        vetor[i].autor[strcspn(vetor[i].autor, "\n")] = '\0';
+
+        printf("Editora: ");
+        fgets(vetor[i].editora, 100, stdin);
+        vetor[i].editora[strcspn(vetor[i].editora, "\n")] = '\0';
+
+        printf("Ano de Publicação: ");
+        scanf("%d", &vetor[i].anoPublicacao);
+
+        printf("Quantidade de Páginas: ");
+        scanf("%d", &vetor[i].qtdPaginas);
+        getchar(); 
+    }
+}
+
+void imprimirVetor(Livro vetor[], int n) {
+    printf("\n--- Lista de Livros ---\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d. %s | %s | %s | %d | %d páginas\n", 
+            i, vetor[i].titulo, vetor[i].autor, vetor[i].editora, 
+            vetor[i].anoPublicacao, vetor[i].qtdPaginas);
+    }
+}
+
+int buscarPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(vetor[i].titulo, tituloBusca) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void ordenarPorTitulo(Livro vetor[], int n) {
+    Livro temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-1-i; j++) {
+            if (strcmp(vetor[j].titulo, vetor[j+1].titulo) > 0) {
+                temp = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = temp;
+            }
+        }
+    }
+}
+
+int buscaBinariaPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
+    int esq = 0, dir = n - 1;
+
+    while (esq <= dir) {
+        int meio = (esq + dir) / 2;
+        int cmp = strcmp(vetor[meio].titulo, tituloBusca);
+
+        if (cmp == 0) return meio;
+        else if (cmp < 0) esq = meio + 1;
+        else dir = meio - 1;
+    }
+
+    return -1;
 }
