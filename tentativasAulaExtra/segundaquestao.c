365ad56665c5e0482a9f6e2ee4fd75af7e13f373
Diogo Carvalho <150797053+Dioguitin@users.noreply.github.com>
2025-05-31 22:49:04 -0300

    Update segundaquestao.c

--- tentativasAulaExtra/segundaquestao.c
+++ tentativasAulaExtra/segundaquestao.c
@@ -1,76 +1,120 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TAM_NOME 30
-#define TAM_TEMP 256
+#define MAX_LIVROS 100
 
-struct Livro {
-    char titulo[TAM_NOME];
-    char autor[TAM_NOME];
-    char editora[TAM_NOME];
-    int anoPublicacao, qtdPaginas;
-};
+typedef struct {
+    char titulo[100];
+    char autor[100];
+    char editora[100];
+    int anoPublicacao;
+    int qtdPaginas;
+} Livro;
 
-void lerStr(char *str, int count);
-void lerLivro(struct Livro *livro);
-void imprimirLivro(struct Livro livro);
 
-int main(){
+void preencherVetor(Livro vetor[], int *n) {
+    printf("Quantos livros deseja cadastrar? ");
+    scanf("%d", n);
+    getchar(); 
 
-struct Livro livro1, livro2;
-printf("----- Digitação dos dados do Livro 1 -----\n");
-lerLivro(&livro1);
-printf("----- Digitação dos dados do Livro 2 -----\n");
-lerLivro(&livro2);
-printf("----- Impressão dados do Livro 1 -----\n");
-imprimirLivro(livro1);
-printf("----- Impressão dados do Livro 2 -----\n");
-imprimirLivro(livro2);
-return 0;
-}
+    for (int i = 0; i < *n; i++) {
+        printf("\nLivro %d:\n", i + 1);
+        printf("Título: ");
+        fgets(vetor[i].titulo, 100, stdin);
+        vetor[i].titulo[strcspn(vetor[i].titulo, "\n")] = '\0';
 
-void lerStr(char *str, int count) {
-  fgets(str, count, stdin);
-  int tam = strlen(str);
-  if (tam > 0 && str[tam - 1] == '\n') {
-    str[tam - 1] = '\0';
-  }
-}
+        printf("Autor: ");
+        fgets(vetor[i].autor, 100, stdin);
+        vetor[i].autor[strcspn(vetor[i].autor, "\n")] = '\0';
 
-void lerLivro(struct Livro *livro) {
-  char tempStr[TAM_TEMP];
-  printf("Digite o nome do livro: \n");
-  lerStr(livro->titulo, TAM_NOME);
-  printf("Digite o nome do autor: \n");
-  lerStr(livro->autor, TAM_NOME);
-  printf("Digite o nome da editora: \n");
-  lerStr(livro->editora, TAM_NOME);
-  printf("Digite o ano de publicação do livro: \n");
-  scanf("%d", &livro->anoPublicacao);
-  printf("Digite a quantidade de páginas: \n");
-  scanf("%d", &livro->qtdPaginas);
-}
- 
-void imprimirLivro(struct Livro livro) {
-  printf("(Livro: %s\nAutor: %s\nEditora: %s\nAno: %d,Quantia de paginas:  %d)\n",
-    livro.titulo,
-    livro.autor,
-    livro.editora,
-    livro.anoPublicacao,
-    livro.qtdPaginas
-  );
+        printf("Editora: ");
+        fgets(vetor[i].editora, 100, stdin);
+        vetor[i].editora[strcspn(vetor[i].editora, "\n")] = '\0';
+
+        printf("Ano de Publicação: ");
+        scanf("%d", &vetor[i].anoPublicacao);
+
+        printf("Quantidade de Páginas: ");
+        scanf("%d", &vetor[i].qtdPaginas);
+        getchar(); 
+    }
 }
 
 
+void imprimirVetor(Livro vetor[], int n) {
+    printf("\n--- Lista de Livros ---\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d. %s | %s | %s | %d | %d páginas\n", 
+            i, vetor[i].titulo, vetor[i].autor, vetor[i].editora, 
+            vetor[i].anoPublicacao, vetor[i].qtdPaginas);
+    }
+}
 
 
+int buscarPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(vetor[i].titulo, tituloBusca) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 
+void ordenarPorTitulo(Livro vetor[], int n) {
+    Livro temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-1-i; j++) {
+            if (strcmp(vetor[j].titulo, vetor[j+1].titulo) > 0) {
+                temp = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = temp;
+            }
+        }
+    }
+}
 
 
-/*
-  scanf("%d", &pessoa->idade); // scanf("%d", &(*pessoa).idade);
-  printf("Digite o peso: ");
-  scanf("%lf", &pessoa->peso); // scanf("%lf", &(*pessoa).peso);
-  printf("Digite a altura: ");
-  */
+int buscaBinariaPorTitulo(Livro vetor[], int n, char tituloBusca[]) {
+    int esq = 0, dir = n - 1;
+
+    while (esq <= dir) {
+        int meio = (esq + dir) / 2;
+        int cmp = strcmp(vetor[meio].titulo, tituloBusca);
+
+        if (cmp == 0) return meio;
+        else if (cmp < 0) esq = meio + 1;
+        else dir = meio - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    Livro livros[MAX_LIVROS];
+    int n;
+
+    preencherVetor(livros, &n);
+
+    printf("\nAntes da ordenação:\n");
+    imprimirVetor(livros, n);
+
+    ordenarPorTitulo(livros, n);
+
+    printf("\nApós ordenação por título:\n");
+    imprimirVetor(livros, n);
+
+    char tituloBusca[100];
+    printf("\nDigite o título do livro para buscar: ");
+    fgets(tituloBusca, 100, stdin);
+    tituloBusca[strcspn(tituloBusca, "\n")] = '\0';
+
+    int pos = buscaBinariaPorTitulo(livros, n, tituloBusca);
+    if (pos != -1) {
+        printf("Livro encontrado na posição %d: %s\n", pos, livros[pos].titulo);
+    } else {
+        printf("Livro não encontrado.\n");
+    }
+
+    return 0;
+}
